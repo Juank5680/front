f32cc7080053391946b75907272d608a4dd29b4d
Juan Pinzon <jcpinzonc@uqvirtual.edu.co>
2023-05-01 19:49:11 -0500

    prueba

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = () => {
     return {
-        SERVER_URL: "back-production-201f.up.railway.app",
+        SERVER_URL: "back-production-8178.up.railway.app",
         // SERVER_URL: "http://localhost:3000",
     };
 };
