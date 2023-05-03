8fd09c708131ca194957f670e902d57c966069f3
Juan Pinzon <jcpinzonc@uqvirtual.edu.co>
2023-05-02 23:37:41 -0500

    Despliegue 3

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = () => {
     return {
-        SERVER_URL: "back-production-8178.up.railway.app",
+        SERVER_URL: "back-software.herokuapp.com",
         // SERVER_URL: "http://localhost:3000",
     };
 };
